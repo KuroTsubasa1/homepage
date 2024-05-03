9455b731ca1f0fea871d6b2014bc28b50866387b
Lasse Harm <lasseharm@web.de>
2024-05-03 12:24:58 +0200

    Updated social row with real links

--- components/socialRow.vue
+++ components/socialRow.vue
@@ -3,15 +3,19 @@
 </script>
 
 <template>
-
   <div class="text-center text-2xl font-bold">
     <div class="flex justify-center items-center text-3xl">
-      <Icon name="mdi:mail-outline" class="mr-3 text-primary"/>
-      <Icon name="mdi:instagram" class="mr-3 text-primary"/>
-      <Icon name="mdi:github" class=" text-primary"/>
+      <a href="mailto:lasseharm@web.de">
+        <Icon name="mdi:mail-outline" class="mr-3 text-primary"/>
+      </a>
+      <a href="https://www.instagram.com/3d.print.shop.harm/">
+        <Icon name="mdi:instagram" class="mr-3 text-primary"/>
+      </a>
+      <a href="https://github.com/KuroTsubasa1">
+        <Icon name="mdi:github" class=" text-primary"/>
+      </a>
     </div>
   </div>
-
 </template>
 
 <style scoped>
